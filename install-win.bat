4ad3bb1ed3b9a01d4267cb55fc75f89004b65d16
unknown <rgoliveira@inf.ufrgs.br>
2015-03-11 16:54:52 -0300

    adding setup script for windows bonus: registry file to add context menu option "Open ViM here" to folders

---
+++ install-win.bat
@@ -0,0 +1,51 @@
+@ECHO OFF
+::Test If script has Admin Priviledges/is elevated
+:: see http://stackoverflow.com/questions/4051883/batch-script-how-to-check-for-admin-rights
+net session >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO You need to run this with elevated rights...
+    PAUSE
+    EXIT /B 1
+)
+
+set dothome=%~dp0
+
+:::::::::::::::
+:: ViM Setup ::
+:::::::::::::::
+echo ViM dotfiles setup in progress...
+
+set vimdir=%dothome%\vim
+
+:: cleanup
+echo   - Cleaning up old files/links (if exist)...
+del   /Q /S %USERPROFILE%\_vimrc    >nul 2>&1
+del   /Q /S %USERPROFILE%\_gvimrc   >nul 2>&1
+rmdir /Q /S %USERPROFILE%\vimfiles  >nul 2>&1
+
+:: set soft links
+echo   - Creating new links...
+mklink    %USERPROFILE%\_vimrc    %vimdir%\.vimrc   >nul 2>&1
+mklink    %USERPROFILE%\_gvimrc   %vimdir%\.gvimrc  >nul 2>&1
+mklink /D %USERPROFILE%\vimfiles  %vimdir%\.vim     >nul 2>&1
+
+:: set context menu for folders
+echo.
+ver>nul
+if %errorlevel% EQU 0 (
+  choice /m "Want to add 'Open ViM here' context menu entries for folders?" /c YN
+  IF ERRORLEVEL 2 goto :ctxNope
+  IF ERRORLEVEL 1 cmd /c %dothome%\win_register-context-options.reg
+)
+:ctxNope
+
+:::::::::
+:: fin ::
+:::::::::
+echo.
+echo Done!
+echo.
+pause
+
+exit /b 0
+
