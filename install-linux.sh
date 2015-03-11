70483521e83d24cb82dee548c8e06cf3f2686c4b
Ricardo Oliveira <rgoliveira@inf.ufrgs.br>
2015-03-11 18:24:10 -0300

    fix linux install

--- install-linux.sh
+++ install-linux.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
-set stowcmd="stow"
-hash $stowcmd 2>/dev/null || { 
-  echo >&2 "I require $stowcmd, but it's not installed.";
-  echo >&2 "Please install it because 1) I need it and 2) it's awesome.";
+
+hash stow 2>/dev/null || { 
+  echo >&2 "I require stow, but it's not installed.";
+  echo >&2 "Please install it because";
+  echo >&2 "    1) I need it; and";
+  echo >&2 "    2) it's awesome.";
   echo >&2 "Aborting.";
   exit 1;
 }
 
-set dothome=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-stow -t ~ $dothome\vim
+dothome=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+rm -rf ~/.vim ~/.vimrc ~/.gvimrc
+stow -t ~ -d $dothome vim
 
